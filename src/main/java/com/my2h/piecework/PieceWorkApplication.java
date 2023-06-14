19f9f60ca13bb08d2aef27c792ac032c72091be0
pengsong <310433620@qq.com>
2023-06-14 11:08:34 +0800

    第一次提交

---
+++ src/main/java/com/my2h/piecework/PieceWorkApplication.java
@@ -0,0 +1,13 @@
+package com.my2h.piecework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieceWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PieceWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my2h/piecework/PieceWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my2h.piecework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieceWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
